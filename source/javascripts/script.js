0f67cb1939c64cdbb298639e8d5ebe978fb67b72
Noel Delgado <auhcsei@gmail.com>
2013-09-30 22:09:23 -0500

    example

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,13 +1,36 @@
 //= require_tree .
 
-(function(){
+(function() {
 
-    new PSDGuides({
-        show        : false,
-        canvasWidth : 500,
-        lineColor   : "#0fc",
-        xGuides     : [10, 60],
-        yGuides     : [37, 70, 25, 55, 30, 835, 135]
-    });
+    var sample = {
+
+        init : function() {
+            this.guides;
+            this.actionBtn = document.querySelector('.js-activate-lib'),
+            this.actionBtn.addEventListener('click', this.activateGuides, false);
+        },
+
+        activateGuides : function activateGuides (event) {
+            event.preventDefault();
+
+            sample.guides = new PSDGuides({
+                show        : true,
+                canvasWidth : 500,
+                lineColor   : "#0fc",
+                xGuides     : [10, 60],
+                yGuides     : [37, 70, 25, 55, 30, 835, 135]
+            });
+
+            sample.actionBtn.removeEventListener('click', sample.activateGuides, false);
+            sample.actionBtn.addEventListener('click', sample.toggleGuides, false);
+        },
+
+        toggleGuides : function toggleGuides (event) {
+            event.preventDefault();
+            sample.guides.toggle();
+        }
+    };
+
+    sample.init();
 
 })();
